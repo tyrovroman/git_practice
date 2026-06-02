0bac7fd1559f318398df17a2fd8addee3fcf04c7
Roman Tyrov <tyrov.roman@gmail.com>
2026-06-02 16:56:05 +0700

    Update Main.java

--- src/main/java/com/tyrovroman/Main.java
+++ src/main/java/com/tyrovroman/Main.java
@@ -4,7 +4,7 @@ package com.tyrovroman;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    private static String message = "Hello, world!!!";
+    private static String message = "Hello, world";
     
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
